5d56f2d5562834a9cef1b318f1d0e45910e37b62
Dariusz Tomaszewski <linuxiarz1624pl@gmail.com>
2021-03-07 10:59:34 +0100

    Add pygame as a sound player

--- testsound.py
+++ testsound.py
@@ -1,40 +1,9 @@
 #!/usr/bin/env python3
 
-# from playsound import playsound
 import Adafruit_MPR121.MPR121 as MPR121
-import logging
-# import threading
 import pygame
 import time
 import sys
-
-
-# def thread_function(filename):
-#     logging.info("Sound %s: play", filename)
-#     playsound(filename)
-#     logging.info("Sound %s: stop", filename)
-
-
-# x1 = threading.Thread(target=thread_function, args=("frogs2.wav",))
-# x2 = threading.Thread(target=thread_function, args=("moor6.wav",))
-def sound1():
-    pass
-
-
-def sound2():
-    pass
-
-
-def playsound(touched_pin):
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
-
-    if touched_pin == 0:
-        sound1()
-    if touched_pin == 11:
-        sound2()
-
 
 cap = MPR121.MPR121()
 
@@ -45,15 +14,55 @@ if not cap.begin():
 pygame.mixer.pre_init(44100, -16, 12, 512)
 pygame.init()
 
+SOUND_MAPPING = {
+    0: './Sounds/Animal/Bird.wav',
+    1: './Sounds/Animal/Cricket.wav',
+    2: './Sounds/Animal/Dog1.wav',
+    3: './Sounds/Animal/Dog2.wav',
+    4: './Sounds/Animal/Duck.wav',
+    5: './Sounds/Animal/Goose.wav',
+    6: './Sounds/Animal/Horse.wav',
+    7: './Sounds/Animal/Kitten.wav',
+    8: './Sounds/Animal/Meow.wav',
+    9: './Sounds/Animal/Owl.wav',
+    10: './Sounds/Animal/Rooster.wav',
+    11: './Sounds/Animal/WolfHowl.wav',
+}
+
+sounds = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for key, soundfile in SOUND_MAPPING.items():
+    sounds[key] = pygame.mixer.Sound(soundfile)
+    sounds[key].set_volume(1)
+
 print('Press Ctrl-C to quit.')
+
+# For RPi
 last_touched = cap.touched()
 while True:
     current_touched = cap.touched()
     for i in range(12):
         pin_bit = 1 << i
         if current_touched & pin_bit and not last_touched & pin_bit:
-            playsound(i)
+            print('{0} touched!'.format(i))
+            if (sounds[i]):
+                sounds[i].play()
         if not current_touched & pin_bit and last_touched & pin_bit:
-            pass
+            print('{0} released!'.format(i))
+
     last_touched = current_touched
     time.sleep(0.1)
+
+
+# For PC
+# touching = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# while True:
+#     i = int(input())
+#     if touching[i] == 0:
+#         touching[i] = 1
+#         print('{0} touched!'.format(i))
+#         if (sounds[i]):
+#             sounds[i].play()
+#     else:
+#         touching[i] = 0
+#         print('{0} released!'.format(i))
