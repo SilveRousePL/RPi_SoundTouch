2eb272acec082ac9efdfff70da9cbfa7e71c8e2e
Dariusz Tomaszewski <linuxiarz1624pl@gmail.com>
2021-03-07 10:36:04 +0100

    Add sounds from scratch, conf vscode

--- testsound.py
+++ testsound.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
-from playsound import playsound
+# from playsound import playsound
 import Adafruit_MPR121.MPR121 as MPR121
 import logging
-import threading
+# import threading
+import pygame
 import time
 import sys
 
 
-def thread_function(filename):
-    logging.info("Sound %s: play", filename)
-    playsound(filename)
-    logging.info("Sound %s: stop", filename)
+# def thread_function(filename):
+#     logging.info("Sound %s: play", filename)
+#     playsound(filename)
+#     logging.info("Sound %s: stop", filename)
 
 
-x1 = threading.Thread(target=thread_function, args=("frogs2.wav",))
-x2 = threading.Thread(target=thread_function, args=("moor6.wav",))
+# x1 = threading.Thread(target=thread_function, args=("frogs2.wav",))
+# x2 = threading.Thread(target=thread_function, args=("moor6.wav",))
+def sound1():
+    pass
+
+
+def sound2():
+    pass
 
 
 def playsound(touched_pin):
@@ -23,10 +30,10 @@ def playsound(touched_pin):
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
 
-    if touched_pin is 0:
-        x1.start()
-    if touched_pin is 11:
-        x2.start()
+    if touched_pin == 0:
+        sound1()
+    if touched_pin == 11:
+        sound2()
 
 
 cap = MPR121.MPR121()
@@ -34,6 +41,9 @@ cap = MPR121.MPR121()
 if not cap.begin():
     print('Error initializing MPR121.  Check your wiring!')
     sys.exit(1)
+
+pygame.mixer.pre_init(44100, -16, 12, 512)
+pygame.init()
 
 print('Press Ctrl-C to quit.')
 last_touched = cap.touched()
